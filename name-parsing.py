61a24a6efd3d49b1d506a8c110d42289cf029752
JaymeeDEV <jamiekellybrown@gmail.com>
2020-01-21 00:44:28 +0000

    Pull same occurrences from both text files

--- name-parsing.py
+++ name-parsing.py
@@ -1,3 +1,10 @@
-with open('first-names.txt', 'r') as names:
-    first_names = names.read()
-    print(first_names)
+with open('first-names.txt') as f:
+    first_names = f.readlines()
+
+with open('oliver-twist.txt') as f:
+    oliver_twist = f.read()
+
+with open('occurrences.txt', 'a') as f:
+    for name in first_names:
+        if name in oliver_twist:
+            f.write(name)
